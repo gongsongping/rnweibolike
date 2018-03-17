1fbe3ba278d03a096f684b3cd36631ac762b47f7
binh <binh@binhs-MacBook-Pro.local>
2018-03-18 00:42:16 +0800

    tweetslist

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import {observer} from 'mobx-react/native'
 import { StackNavigator, TabNavigator, TabBarBottom} from 'react-navigation';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { Input, Badge, SearchBar } from 'react-native-elements';
+import { Input, Badge, SearchBar, Avatar } from 'react-native-elements';
 
 
 // @observer
@@ -235,7 +235,7 @@ const MineScreen = observer(class MineScreen extends React.Component {
         <FlatList
           data={this.list}
           renderItem={({item,index}) => <Text style={styles.item} key={index}>{item.key}</Text>}
-          keyExtractor={(item, index) => index} 
+          keyExtractor={(item, index) => index.toString()} 
           onEndReachedThreshold={0.5}
           onEndReached={this.loadMore}
           refreshing={this.refreshing}
@@ -246,8 +246,98 @@ const MineScreen = observer(class MineScreen extends React.Component {
   }
 })
 
+const TweetListScreen = observer(class TweetListScreen extends  React.Component{
+ list = [
+    {
+      name: '聪聪',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+      images: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+      tweet: '评论里留下你的〖生日和所在地〗。看到有缘的就找ta聊聊呗！遇到同一天生日的，互粉一个，以后过生日就不孤单啦！'
+    },
+    {
+      name: '慕小白',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      images: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      tweet: '说一件你经历的，最幸运爆棚的事！！！让我们沾沾喜气！出生前追上卵子那次不算 ​​​ ​​​​'
+    },
+    {
+      name: '龙仔',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg',
+      images: 'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg',
+      tweet: '低潮时没有，但生死悠关时有一句话很给力，13年出车祸的那天，我被120送到医院，快失去意识的时候，我感觉我的眼睛像虚焦的镜头一样，什么都是模糊的，能看的稍微清楚点的以及现在唯一能记在脑海里的就是医生和护士的白大褂。然后模糊的听见一句话，不知道是谁说的“赶紧的吧！你妈买彩票中奖了！” '
+    },
+    {
+      name: '妻弄',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
+      images: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
+      tweet: '以前我很怕得罪人，不敢迟到，不敢要求，不敢说错话，怕冷场，怕对方不高兴，诚惶诚恐的面对所有人。遗憾的是，我并没有得到相应的尊重。后来，我开始宠爱和迁就自己，你迟到我先走，不想做绝不勉强，冷场就玩手机，多顾个人感受，其他人关我屁事。然后发现，我不但被重视，而且更开心了。——易术 '
+    }
+  ]
+
+  constructor(props){
+    super(props)
+    this.loadMore = this.loadMore.bind(this)
+    this.refresh = this.refresh.bind(this)
+    extendObservable(this,{
+      tweets: this.list
+    })
+  }
+  loadMore(){
+    // console.log('loading'); 
+    this.tweets = this.tweets.concat(this.list)
+  }
+  refreshing = false
+  refresh(){
+    this.refreshing = true
+    this.tweets = this.list
+    this.refreshing = false
+    
+  }
+
+  render(){
+    return (
+      <View style={{padding:5}}>
+         <FlatList
+          data={this.tweets}
+          renderItem={({item,index}) => 
+            <View key={index} style={{paddingBottom:8}}>
+              <View style={{flexDirection:'row',paddingBottom:5}}>
+                <View style={{flex:1}}>
+                  <Avatar
+                    small
+                    rounded
+                    source={{uri: item.avatar_url}}
+                    activeOpacity={0.7}
+                  />
+                </View>
+                <View style={{flex:6}}>
+                  <Text>{item.name}</Text>
+                  <Text style={{fontSize:12,paddingTop:5}}>2-16</Text>
+                </View>
+              </View>
+              <View style={{paddingTop:5, paddingBottom:10}}>
+                <Text style={{lineHeight: 18}}>{item.tweet}</Text>
+              </View>
+              <View style={{flexDirection:'row',  padding:8, justifyContent:'space-around', borderTopColor:'lightgrey', borderTopWidth:0.5, borderBottomColor:'lightgrey', borderBottomWidth:10}}>
+                <Icon name='retweet' color='grey' />
+                <Icon name='comment' color='grey' />
+                <Icon name='thumbs-o-up' color='grey' />
+              </View>
+            </View>}
+          keyExtractor={(item, index) => index.toString()} 
+          onEndReachedThreshold={0.5}
+          onEndReached={this.loadMore}
+          refreshing={this.refreshing}
+          onRefresh={this.refresh}
+        />
+      </View>
+    )
+  }
+
+})
+
 const HomeStack = StackNavigator({
-  Home: { screen: HomeScreen },
+  Home: { screen: TweetListScreen },
   Details: { screen: DetailsScreen, 
     navigationOptions: {
       title: '设备总览',
@@ -266,7 +356,7 @@ export default TabNavigator(
   {
     首页: { 
       screen: HomeStack,
-      // path: '/home',
+      path: '/home',
       navigationOptions: ({ navigation }) => ({
         // tabBarLabel: 'Home',
         tabBarIcon: ({ focused, tintColor }) => {
@@ -276,7 +366,7 @@ export default TabNavigator(
     },
     我的: { 
       screen: MineStack,
-      // path: '/mine',
+      path: '/mine',
       navigationOptions: ({ navigation }) => ({
         // tabBarLabel: 'Mine',
         tabBarIcon: ({ focused, tintColor }) => {
@@ -286,19 +376,6 @@ export default TabNavigator(
     },
   },
   {
-    // navigationOptions: ({ navigation }) => ({
-    //   tabBarIcon: ({ focused, tintColor }) => {
-    //     const { routeName } = navigation.state;
-    //     let iconName;
-    //     if (routeName === '首页') {
-    //       iconName = `ios-information-circle${focused ? '' : '-outline'}`;
-    //     } else if (routeName === '我的') {
-    //       iconName = `ios-options${focused ? '' : '-outline'}`;
-    //     }
-    //     // You can return any component that you like here! We usually use an
-    //     return <Ionicons name={iconName} size={25} color={tintColor} />;
-    //   }
-    // }),
     tabBarOptions: {
       activeTintColor: 'tomato',
       inactiveTintColor: 'gray',
